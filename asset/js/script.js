beec41d14045e532f378f14367a60d291f1aac1d
naveenpradeep0987@gmail.com <Naveenpradeep02.noreply.github.com>
2025-02-14 17:48:31 +0530

    update

---
+++ asset/js/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".auto-type", {
+  strings: [
+    "Digital Marketing",
+    "Social Media Marketing",
+    "LEAD GENERATION",
+    "SEO",
+    "WEBSITE DESIGNING",
+    "BRANDING",
+  ],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});

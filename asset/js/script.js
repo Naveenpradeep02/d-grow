9bc87c5fba522e2727bdf6bb5527c58ea7c33f7a
naveenpradeep0987@gmail.com <Naveenpradeep02.noreply.github.com>
2025-02-15 13:42:55 +0530

    update

--- asset/js/script.js
+++ asset/js/script.js
@@ -11,3 +11,12 @@ var typed = new Typed(".auto-type", {
   backSpeed: 150,
   loop: true,
 });
+
+const text = document.querySelector(".circle-text p");
+
+text.innerHTML = text.innerText
+  .split("")
+  .map(
+    (char, i) => `<span style="transform:rotate(${i * 6}deg)">${char}</span>`
+  )
+  .join("");

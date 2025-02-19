33f0dc4cb747761818b59ad7f5672414efa2d4ab
naveenpradeep0987@gmail.com <Naveenpradeep02.noreply.github.com>
2025-02-19 17:43:15 +0530

    update

--- asset/js/script.js
+++ asset/js/script.js
@@ -20,48 +20,6 @@ text.innerHTML = text.innerText
     (char, i) => `<span style="transform:rotate(${i * 5.7}deg)">${char}</span>`
   )
   .join("");
-// let sliderContainer = document.querySelector("#sliderContainer");
-// let slider = document.querySelector("#slider");
-// let cards = document.querySelectorAll(".list-card");
-// let elementShow = 3;
-// let sliderContainerWidth = sliderContainer.clientWidth;
-
-// if (document.body.clientWidth <= 768) {
-//   elementShow = 1;
-// }
-
-// let cardWidth = sliderContainerWidth / elementShow;
-// let cardMargin = 20;
-
-// slider.style.width = (cardWidth + cardMargin) * cards.length + "px";
-// slider.style.transition = "margin";
-// slider.style.transitionDuration = "1s";
-
-// cards.forEach((card) => {
-//   card.style.width = cardWidth + "px";
-//   card.style.marginRight = cardMargin + "px";
-// });
-
-// if (!slider.style.marginLeft) {
-//   slider.style.marginLeft = "0px";
-// }
-
-// const next = () => {
-//   let currentMargin = parseInt(slider.style.marginLeft) || 0;
-//   let maxMargin = -((cardWidth + cardMargin) * (cards.length - elementShow));
-
-//   if (currentMargin > maxMargin) {
-//     slider.style.marginLeft = `${currentMargin - (cardWidth + cardMargin)}px`;
-//   }
-// };
-
-// const prev = () => {
-//   let currentMargin = parseInt(slider.style.marginLeft) || 0;
-
-//   if (currentMargin < 0) {
-//     slider.style.marginLeft = `${currentMargin + (cardWidth + cardMargin)}px`;
-//   }
-// };
 
 let sliderContainer = document.querySelector("#sliderContainer");
 let slider = document.querySelector("#slider");
@@ -118,3 +76,5 @@ sliderContainer.addEventListener("mouseenter", () => {
 sliderContainer.addEventListener("mouseleave", () => {
   autoSlide = setInterval(next, 5000);
 });
+
+// menu
